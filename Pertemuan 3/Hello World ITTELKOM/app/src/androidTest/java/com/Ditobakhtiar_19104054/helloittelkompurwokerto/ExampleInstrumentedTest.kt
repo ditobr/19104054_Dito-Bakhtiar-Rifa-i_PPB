4479fbe91d1d090547186469fe6b50b4488c740f
ditobr <72486631+ditobr@users.noreply.github.com>
2021-12-10 12:37:19 +0700

    Update ExampleInstrumentedTest.kt

--- Pertemuan 3/Hello World ITTELKOM/app/src/androidTest/java/com/Ditobakhtiar_19104054/helloittelkompurwokerto/ExampleInstrumentedTest.kt
+++ Pertemuan 3/Hello World ITTELKOM/app/src/androidTest/java/com/Ditobakhtiar_19104054/helloittelkompurwokerto/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.fajarahadi_19104007.helloittelkompurwokerto
+package com.Ditobakhtiar_19104054.helloittelkompurwokerto
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
