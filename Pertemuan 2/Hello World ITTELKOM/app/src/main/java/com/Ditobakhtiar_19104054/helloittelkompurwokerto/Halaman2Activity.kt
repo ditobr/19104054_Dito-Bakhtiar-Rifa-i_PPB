0e04c23a9df1b3d841c285ee1dca01261f328464
ditobr <72486631+ditobr@users.noreply.github.com>
2021-12-10 11:46:48 +0700

    Update Halaman2Activity.kt

--- Pertemuan 2/Hello World ITTELKOM/app/src/main/java/com/Ditobakhtiar_19104054/helloittelkompurwokerto/Halaman2Activity.kt
+++ Pertemuan 2/Hello World ITTELKOM/app/src/main/java/com/Ditobakhtiar_19104054/helloittelkompurwokerto/Halaman2Activity.kt
@@ -1,4 +1,4 @@
-package com.fajarahadi_19104007.helloittelkompurwokerto
+package com.Ditobakhtiar_19104054.helloittelkompurwokerto
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
